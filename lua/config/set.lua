c97c546f4a66f9e6e27b54ea43f1e5a1629c8393
Tiffany <tiffzhou@umich.edu>
2026-02-20 18:47:41 -0500

    add gitsigns keybinds and change curr line highlight

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -2,6 +2,7 @@ vim.opt.wrap = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,5 +2,45 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = "BufReadPre",
-	opts = {},
+	opts = {
+		on_attach = function(bufnr)
+			local gs = package.loaded.gitsigns
+
+			local function map(mode, l, r, desc)
+				vim.keymap.set(mode, l, r, { buffer = bufnr, silent = true, desc = desc })
+			end
+
+			-- navigation
+			map("n", "]c", function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "]c", bang = true })
+				else
+					gs.nav_hunk("next")
+				end
+			end, "Next Git Hunk")
+
+			map("n", "[c", function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "[c", bang = true })
+				else
+					gs.nav_hunk("prev")
+				end
+			end, "Previous Git Hunk")
+
+			-- hunk preview
+			map("n", "<leader>gg", gs.preview_hunk, "Preview Git Hunk")
+
+			-- staging and unstaging (normal mode - whole hunk under cursor)
+			map("n", "<leader>gs", gs.stage_hunk, "(Un)Stage Git Hunk")
+			map("n", "<leader>grr", gs.reset_hunk, "Reset Git Hunk")
+
+			-- partial staging (visual mode - only highlighted lines)
+			map("v", "<leader>gs", function()
+				gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+			end, "(Un)Stage Git Selection")
+			map("v", "<leader>gr", function()
+				gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+			end, "Reset Git Selection")
+		end,
+	},
 }
